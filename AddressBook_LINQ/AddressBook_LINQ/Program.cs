e19adada3cc9ddcff1879fe15d99eaf854d940e3
sagunaukade <sagunaukade96@gmail.com>
2022-03-05 11:53:33 +0530

    [Saguna] Welcome Message

---
+++ AddressBook_LINQ/AddressBook_LINQ/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBook_LINQ
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Address Book LINQ");
+            Console.ReadLine();
+        }
+    }
+}
